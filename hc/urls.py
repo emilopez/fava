ab5ec6063946dd65f1b91d11b213675944040532
Emiliano <emiliano.lopez@gmail.com>
2016-11-27 02:39:01 -0300

    cambio template bootstrap a admin v2

--- hc/urls.py
+++ hc/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.pacientes, name='pacientes'),
+    url(r'^paciente/(?P<pk>\d+)/$', views.paciente_detalle, name='paciente_detalle'),
 ]
--- hc/views.py
+++ hc/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Paciente, Profesional
 
 # Create your views here.
@@ -6,3 +6,7 @@ def pacientes(request):
     pacientes = Paciente.objects.order_by('apellido')
     #Question.objects.order_by('-pub_date')[:5]
     return render(request, 'hc/pacientes.html', {'pacientes': pacientes})
+
+def paciente_detalle(request, pk):
+    paciente = get_object_or_404(Paciente, pk=pk)
+    return render(request, 'hc/paciente.html', {'paciente': paciente})
